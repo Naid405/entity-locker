1c28f58acf3b6431739ad7de2457ce1a2f64babb
Naid405 <ivan405106@gmail.com>
2016-12-30 02:27:55 +0400

    Lock reentry test

--- src/test/java/iasemenov/EntityLockerTests.java
+++ src/test/java/iasemenov/EntityLockerTests.java
@@ -128,7 +128,26 @@ public class EntityLockerTests {
 
     @Test
     public void testLockReentry() throws Exception {
+        Long id = 1L;
 
+        try {
+            locker.lockEntity(id);
+            locker.lockEntity(id, 10, TimeUnit.MILLISECONDS);
 
+            Future<Boolean> task1 = executor.submit(() -> {
+                try {
+                    return locker.lockEntity(id, 100, TimeUnit.MILLISECONDS);
+                } catch (InterruptedException e) {
+                    logger.fatal("Should not have happened", e);
+                    return true;
+                }
+            });
+            Assert.assertFalse(task1.get());
+
+        } catch (InterruptedException e) {
+            logger.fatal("Should not have happened", e);
+        } finally {
+            locker.unlockEntity(id);
+        }
     }
 }
