4f2cd2ac5d8875dc0c6602e16841be3e22cc344d
Naid405 <ivan405106@gmail.com>
2017-01-02 17:43:32 +0400

    Java doc for lock holder

--- src/main/java/iasemenov/locker/LockHolder.java
+++ src/main/java/iasemenov/locker/LockHolder.java
@@ -2,6 +2,9 @@ package iasemenov.locker;
 
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * Used to store the lock in HashingEntityLocker
+ */
 public interface LockHolder {
     ReentrantLock get();
 }
